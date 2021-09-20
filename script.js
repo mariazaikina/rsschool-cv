30c0283bbda27db2b9e6684016fed5fcb9d57928
mariazaikina <mashtakova93@gmail.com>
2021-09-21 00:22:36 +0400

    fix: validate fixes

--- script.js
+++ script.js
@@ -25,6 +25,3 @@ console.info(check);
 }
 myOpinion();
 });
-document.querySelectorAll("code").forEach(function(element) {
-   element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-});
